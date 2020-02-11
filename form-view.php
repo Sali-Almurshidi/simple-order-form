8d73927973ebe9ddc68de1b68367973d69166954
sali <salialmurshidi@gmail.com>
2020-02-11 16:58:32 +0100

    done with 4 and 5

--- form-view.php
+++ form-view.php
@@ -6,7 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>Order food & drinks</title>
 </head>
@@ -15,54 +16,57 @@
     <h1>Order food in restaurant "the Personal Ham Processors"</h1>
     <nav>
         <ul class="nav" method="get">
-            <li class="nav-item"  >
-                <a class="nav-link active" href="?food=1"  >Order food</a>
+            <li class="nav-item">
+                <a class="nav-link active" href="?food=1">Order food</a>
             </li>
-            <li class="nav-item" >
-                <a class="nav-link" href="?food=0" >Order drinks</a>
+            <li class="nav-item">
+                <a class="nav-link" href="?food=0">Order drinks</a>
             </li>
         </ul>
     </nav>
-    <form method="post" >
+    <form method="post">
         <?php echo $orderSend; ?>
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="email">E-mail:</label>
-                <input type="email" placeholder="user@gmail.com" id="email" name="email" class="form-control" value="<?php echo ($_POST['email']) ?> "/>
-                    <?php echo $resultCheckEmail ;?>
+                <input type="email" placeholder="user@gmail.com" id="email" name="email" class="form-control"
+                       value="<?php echo $emailAddress ?> "/>
+                <?php echo $resultCheckEmail; ?>
             </div>
             <div></div>
         </div>
 
         <fieldset>
             <legend>Address</legend>
-                <?php
-                echo $AddressFileds;
-                ?>
+            <?php
+            echo $AddressFileds;
+            ?>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control" value="<?php echo ($_POST['street']) ?> ">
+                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $street ?> ">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo ($_POST['streetnumber']) ?> ">
-                        <?php
-                        echo $falseStreetNumber;
-                        ?>
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control"
+                           value="<?php echo $streetnumber; ?> ">
+                    <?php
+                    echo $falseStreetNumber;
+                    ?>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control" value="<?php echo ($_POST['city']) ?> ">
+                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $city; ?> ">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo ($_POST['zipcode']) ?> ">
-                        <?php
-                        echo $falseZipcode;
-                        ?>
+                    <input type="text" id="zipcode" name="zipcode" class="form-control"
+                           value="<?php echo $zipcode; ?> ">
+                    <?php
+                    echo $falseZipcode;
+                    ?>
                 </div>
             </div>
         </fieldset>
@@ -71,20 +75,23 @@
             <legend>Products <?php echo $listName ?></legend>
             <?php echo $selectNothing ?>
 
-            <?php foreach ($productsFood AS $i => $product): ?>
+            <?php
+
+            //if($listName == "Food"){
+            foreach ($products AS $i => $product): ?>
                 <label>
                     <input type="checkbox" value="<?php echo $product['name'] ?>"
-                        <?php if(isset($_POST['products'][$i]) ){
+                        <?php if (isset($_POST['products'][$i])) {
                             echo "checked='checked'";
                         }
-                             ?>
+                        ?>
                            name="products[<?php echo $i ?>]"
                     /> <?php echo $product['name'] ?> -
                     &euro; <?php echo number_format($product['price'], 2) ?></label><br
 
                 />
             <?php endforeach;
-            echo $totalFoodValue; ?>
+            ?>
 
 
         </fieldset>
@@ -100,8 +107,14 @@
         text-align: center;
     }
 </style>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -10,30 +10,53 @@ error_reporting(E_ALL);*/
 
 //we are going to use session variables so we need to enable sessions
 session_start();
-$resultCheckEmail = null;
-$emailAddress = null;
-$AddressFileds = null;
-$falseStreetNumber = null;
-$falseZipcode = null;
-$selectNothing = null;
-$selectFood = array();
-$totalFoodValue = 0.0;
-$orderSend = null;
-$listName = " Food";
-$productsFood = [
+$emailAddress = $streetnumber = $street = $city = $zipcode = $listName = null;
+$totalValue = 0.0;
+$products = [
     ['name' => 'Club Ham', 'price' => 3.20],
     ['name' => 'Club Cheese', 'price' => 3],
     ['name' => 'Club Cheese & Ham', 'price' => 4],
     ['name' => 'Club Chicken', 'price' => 4],
     ['name' => 'Club Salmon', 'price' => 5]
-]; // food array
-$productsDrinks = [
-    ['name' => 'Cola', 'price' => 2],
-    ['name' => 'Fanta', 'price' => 2],
-    ['name' => 'Sprite', 'price' => 2],
-    ['name' => 'Ice-tea', 'price' => 3],
-]; // drinks array
+];
+$resultCheckEmail = null;
+$AddressFileds = null;
+$falseStreetNumber = null;
+$falseZipcode = null;
+$selectNothing = null;
+$selectFood = array();
+//$_SESSION['$totalValue'] = $totalFoodValue;
+$orderSend = null;
 
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if ($_SESSION['emailAddress'] != '' || $_SESSION['streetnumber'] != '' || $_SESSION['street'] != '' || $_SESSION['city'] != '' || $_SESSION['zipcode'] != '') {
+        $emailAddress = $_SESSION['emailAddress'];
+        $streetnumber = $_SESSION['streetnumber'];
+        $street = $_SESSION['street'];
+        $city = $_SESSION['city'];
+        $zipcode = $_SESSION['zipcode'];
+    }
+}// keep the information after go to food or drink page
+
+if ($_GET['food'] == 1) {
+    $listName = " Food";
+    $products = [
+        ['name' => 'Club Ham', 'price' => 3.20],
+        ['name' => 'Club Cheese', 'price' => 3],
+        ['name' => 'Club Cheese & Ham', 'price' => 4],
+        ['name' => 'Club Chicken', 'price' => 4],
+        ['name' => 'Club Salmon', 'price' => 5]
+    ];
+} else {
+    $listName = " Drinks";
+    $products = [
+        ['name' => 'Cola', 'price' => 2],
+        ['name' => 'Fanta', 'price' => 2],
+        ['name' => 'Sprite', 'price' => 2],
+        ['name' => 'Ice-tea', 'price' => 3],
+    ]; // drinks array;
+}
 
 function whatIsHappening()
 {
@@ -47,17 +70,6 @@ function whatIsHappening()
     var_dump($_SESSION);
 }
 
-// get nav number
-if(isset($_GET['food'])){
-    if ($_GET['food'] == 1){
-        echo "this is food";
-        issetOrder($_GET['food'], $productsFood);
-    }else {
-        echo "this is drinks";
-        issetOrder($_GET['food'], $productsDrinks);
-    }
-}
-
 function checkEmail($email)
 {
     filter_var($email, FILTER_VALIDATE_EMAIL);
@@ -68,125 +80,81 @@ function checkEmail($email)
     }
 }// check email address
 
-//issetOrder($foodNav, $drinksNav , $productsFood);
-
-function issetOrder($pageNumber , $products)
+function getEmail($email)
 {
-    $totalFoodValue = 0.0;
-    $totalDrinksValue = 0.0;
-
-    if ($pageNumber == 1) {
-        $listName = " Food" ;
-        echo " this is food";
-
-        if (!empty($_POST['products'])) {
-            $selectFood = $_POST['products'];
-
-            foreach ($selectFood as $key => $foodName) {
-                $totalFoodValue += floatval($products[$key]["price"]);
-            }
-            var_dump($products);
-
-            var_dump($selectFood);
+    $emailAddress = $_SESSION['emailAddress'];
+    if (!empty($email)) {
+        $checkEmail = checkEmail($email);
+        if ($checkEmail == false) {
+            $resultCheckEmail = "<div class=\"alert alert-danger\" role=\"alert\"> This is not valid email address</div>";
+            return false;
         } else {
-            $selectNothing = "<div class=\"alert alert-danger\" role=\"alert\"> You didn't select any product </div>";
+            $resultCheckEmail = "<div class=\"alert alert-success\" role=\"alert\"> This is valid email address</div>";
+            $emailAddress = $email;
+            $_SESSION['emailAddress'] = $emailAddress;
         }
-    }
-    if ($pageNumber == 2) {
-        $listName = "drinks" ;
-        echo " this is drinks";
+    } else {
+        $resultCheckEmail = "<div class=\"alert alert-danger\" role=\"alert\"> Enter email address</div>";
+    }// the email input empty
+}// check if email not empty and valid;
 
-    }
+function getAddress($streetnumber, $street, $city, $zipcode)
+{
+    if (!empty($streetnumber) && !empty($street) && !empty($city) && !empty($zipcode)) {
 
-    if (isset($_POST['order'])) {
-        $emailAddress = null;
-        $streetnumber = null;
-        $street = null;
-        $city = null;
-        $zipcode = null;
+        $numStreet = true;
+        $numZipCode = true;
 
+        if (!is_numeric['streetnumber']) {
+            $falseStreetNumber = "<div class=\"alert alert-danger\" role=\"alert\"> Have to be number</div>";
+            $numStreet = false;
+        } // the street have to be number
 
-        if (!empty($_POST['email'])) {
-            $checkEmail = checkEmail($_POST['email']);
-            if ($checkEmail == false) {
-                $resultCheckEmail = "<div class=\"alert alert-danger\" role=\"alert\"> This is not valid email address</div>";
+        if (!is_numeric['zipcode']) {
+            $falseZipcode = "<div class=\"alert alert-danger\" role=\"alert\"> Have to be number</div>";
+            $numZipCode = false;
+        } // // the zip code have to be number
 
-            } else {
-                $resultCheckEmail = "<div class=\"alert alert-success\" role=\"alert\"> This is valid email address</div>";
-                $emailAddress = $_POST['email'];
-            }
-        } else {
-            $resultCheckEmail = "<div class=\"alert alert-danger\" role=\"alert\"> Enter email address</div>";
-        }// the email input empty
-
-        if (!empty($_POST['streetnumber']) && !empty($_POST['street']) && !empty($_POST['city']) && is_numeric($_POST['zipcode'])) {
+        if ($numStreet = true && $numZipCode = true) {
             $AddressFileds = "<div class=\"alert alert-success\" role=\"alert\"> Your address is complete </div>";
-            $streetnumber = $_POST['streetnumber'];
-            $street = $_POST['street'];
-            $city = $_POST['city'];
-            $zipcode = $_POST['zipcode'];
 
-            $adsressArray = array($streetnumber, $street, $city, $zipcode);
-            //var_dump($adsressArray);
-            $_SESSION['address'] = $adsressArray;
+            $_SESSION['streetnumber'] = $streetnumber;
+            $_SESSION['street'] = $street;
+            $_SESSION['city'] = $city;
+            $_SESSION['zipcode'] = $zipcode;
 
-            if (!is_numeric['streetnumber']) {
-                $falseStreetNumber = "<div class=\"alert alert-danger\" role=\"alert\"> Have to be number</div>";
-            }
-            if (!is_numeric['zipcode']) {
-                $falseZipcode = "<div class=\"alert alert-danger\" role=\"alert\"> Have to be number</div>";
-            }
-
-
-        } else {
-            $AddressFileds = "<div class=\"alert alert-danger\" role=\"alert\"> Complete your address </div>";
-
-            if (!is_numeric['streetnumber']) {
-                $falseStreetNumber = "<div class=\"alert alert-danger\" role=\"alert\"> Have to be number</div>";
-            }
-            if (!is_numeric['zipcode']) {
-                $falseZipcode = "<div class=\"alert alert-danger\" role=\"alert\"> Have to be number</div>";
-            }
-        }// address*/
-
-        /*if (){
-            if (!empty($_POST['productsFood'])) {
-                $selectFood = $_POST['productsFood'];
-
-                foreach ($selectFood as $key => $foodName) {
-                    $totalFoodValue += floatval($products[$key]["price"]);
-                }
-                var_dump($products);
-
-                var_dump($selectFood);
-            } else {
-                $selectNothing = "<div class=\"alert alert-danger\" role=\"alert\"> You didn't select any product </div>";
-            }
-        }// food list
-        else{
-            if (!empty($_POST['productsFood'])) {
-                $selectFood = $_POST['productsFood'];
-
-                foreach ($selectFood as $key => $foodName) {
-                    $totalFoodValue += floatval($products[$key]["price"]);
-                }
-                var_dump($products);
-
-                var_dump($selectFood);
-            } else {
-                $selectNothing = "<div class=\"alert alert-danger\" role=\"alert\"> You didn't select any product </div>";
-            }
-        }// drink list*/
-        $selectNothing = "<div class=\"alert alert-danger\" role=\"alert\"> You didn't select any product </div>";
-
-
-        if (($emailAddress != null) && ($streetnumber != null) && ($street != null) && ($city != null) && ($zipcode != null) && ($selectNothing != null)) {
-            $orderSend = "<div class=\"alert alert-dark\" role=\"alert\"> This order has been sent </div>";
         }
 
-    }// isset for the button
+    } else {
+        $AddressFileds = "<div class=\"alert alert-danger\" role=\"alert\"> Complete your address </div>";
+    }
+}// address
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    getEmail($_POST['email']);
+
+    getAddress($_POST['streetnumber'], $_POST['street'], $_POST['city'], $_POST['zipcode']);
+
+    $totalValue = $_SESSION['totalFoodPrice'] + $_SESSION['totalDrinksPrice'];
+
+
+    if (!empty($_POST['products'])) {
+        $selectProducts = $_POST['products'];
+        //$totalFoodValue = $_SESSION['totalFoodPrice'];
+        $totalValue = 0.0;
+        foreach ($selectProducts as $key => $foodName) {
+            $totalValue += floatval($products[$key]["price"]);
+        }
+        //$_SESSION['totalFoodPrice'] = $totalFoodValue;
+        var_dump($selectProducts);
+    } else {
+        $selectNothing = "<div class=\"alert alert-danger\" role=\"alert\"> You didn't select any food </div>";
+    }
+
+    $orderSend = "<div class=\"alert alert-dark\" role=\"alert\"> This order has been sent and the expected delivery time is 2h and 45m maybe</div>";
+
 }
 
-$totalValue = $totalFoodValue;
 
 require 'form-view.php';
